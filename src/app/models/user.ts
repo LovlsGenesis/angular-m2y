b0490adc0f46cac83e3628d6ed8c99ba294ef191
Guillaume Zeldine <g.zeldine@hotmail.fr>
2021-05-21 15:25:43 -0300

    User model

---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  name: string;
+  cpf: string;
+  account_ids: number[];
+  card_ids: number[];
+  registration_id: string;
+  alias_bank: string;
+  created_at: string;
+}
